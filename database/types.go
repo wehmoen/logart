cac536765a77b292aaf3d51ace12b0b53d5d12cd
Nico Wehmöller <nico.wehmoller@skymavis.com>
2024-07-19 16:59:10 +0200

    Add json tags to event

--- database/types.go
+++ database/types.go
@@ -21,9 +21,9 @@ func GetUserFromContext(c echo.Context) *User {
 }
 
 type Event struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	UserId    primitive.ObjectID `bson:"user_id"`
-	Project   string             `bson:"project"`
-	CreatedAt primitive.DateTime `bson:"created_at"`
-	Data      interface{}        `bson:"data"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	UserId    primitive.ObjectID `bson:"user_id" json:"user_id"`
+	Project   string             `bson:"project" json:"project"`
+	CreatedAt primitive.DateTime `bson:"created_at" json:"created_at"`
+	Data      interface{}        `bson:"data" json:"data"`
 }
